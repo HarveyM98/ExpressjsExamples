6edc56079801658c953fb3e98f2cb73e5dd83410
Harvey Marin <hsmc10n@gmail.com>
2025-06-24 22:11:42 -0500

    get, post, put

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT)
+
 app.get('/', (req, res) => {
     res.send(`
         <h1>Curso Express.js </h1>
@@ -13,6 +21,119 @@ app.get('/', (req, res) => {
         `);
 });
 
-app.listen(PORT, ()=> {
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Mostrar informacion del usuario con ID: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`
+        <h2>Resultados de busqueda: </h2>
+        <p>Termino: ${terms}</p>
+        <p>Categoria: ${category}</p>
+        `)
+});
+
+app.post('/form', (req, res) => {
+    const name = req.body.nombre || 'Anonimo';
+    const email = req.body.email || 'No proporcionado';
+    res.json({
+        message: 'Datos recibidos',
+        data: {
+            name,
+            email
+        }
+    })
+});
+
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+
+    if (!data || Object.keys(data).length == 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    res.status(201).json({
+        message: 'Datos JSON recibidos',
+        data
+    });
+
+});
+
+app.get('/users', (req, res) => {
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error con la conexion de datos.' });
+        }
+        const users = JSON.parse(data);
+        res.json(users);
+    });
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    const name = newUser.name || 'Anonimo';
+    const email = newUser.email || 'No proporcionado';
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!newUser || Object.keys(newUser).length == 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    if (name.length < 3) {
+        return res.status(400).json({ error: 'El nombre debe de ser minimo 3 caracteres' });
+    }
+
+    if (email.length == 0 || !regex.test(email)) {
+        return res.status(400).json({ error: 'El email es invalido' });
+    }
+
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error con la conexion de datos.' });
+        }
+        const users = JSON.parse(data);
+        const lastId = users.length > 0 ? users[users.length - 1].id : 0;
+        const newUser = {
+            id: lastId + 1,
+            name,
+            email,
+        };
+        users.push(newUser);
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error al guardar el usuario.' });
+            }
+            res.status(201).json(newUser);
+        });
+    });
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const { id, ...updatedUser } = req.body;
+
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error con la conexion de datos.' })
+        }
+        let users = JSON.parse(data);
+        users = users.map(user =>
+            user.id === userId ? { ...user, ...updatedUser } : user
+        );
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error al actualizar el usuario.' });
+            }
+            res.json(updatedUser);
+        });
+    });
+});
+
+app.listen(PORT, () => {
     console.log(`Servidor: http://localhost:${PORT}`);
 })
