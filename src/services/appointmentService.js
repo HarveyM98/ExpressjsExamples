b67b880b357f6072ea543d02532f0218e9ecae7a
Harvey Marin <hsmc10n@gmail.com>
2025-06-28 13:17:59 -0500

    appointments API

---
+++ src/services/appointmentService.js
@@ -0,0 +1,14 @@
+const { PrismaClient } = require('../../generated/prisma');
+const prisma = new PrismaClient();
+
+exports.getUserAppointments = async (userId) => {
+    try {
+        const appointments = await prisma.appointment.findMany({
+            where: { userId: parseInt(userId) },
+            include: { timeBlock: true }
+        });
+        return appointments;
+    } catch (error) {
+        throw new Error('Error to obtain appointments information');
+    }
+};
